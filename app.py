dab9601d9dde66cededc6b16e8f5793b256b5bee
장민 <>
2021-09-17 19:00:41 +0900

    눈물의테스트01

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbStock
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/select')
+def select():
+    return render_template('select.html')
+
+# @app.route('/test', methods=['GET'])
+# def test_get():
+#    title_receive = request.args.get('title_give')
+#    print(title_receive)
+#    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
